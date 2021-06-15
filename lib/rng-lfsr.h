215a5792e7e4b43ed164cbc04e500cfecd2df414
langel <knoxburry@gmail.com>
2021-06-14 22:03:28 -0400

    predictable rng and test

--- lib/rng-lfsr.h
+++ lib/rng-lfsr.h
@@ -1,5 +1,6 @@
+#include <stdint.h>
 
-int rng_lfsr_8bit_next(unsigned char *val, int max) {
+int rng_lfsr_8bit_next(uint8_t *val, int max) {
 	*val ^= (*val & 0x07) << 5;
 	*val ^= *val >> 3;
 	*val ^= (*val & 0x03) << 6;
@@ -7,11 +8,11 @@ int rng_lfsr_8bit_next(unsigned char *val, int max) {
 }
 
 int rng8(int max) {
-	static unsigned char val = 1;
+	static uint8_t val = 1;
 	return rng_lfsr_8bit_next(&val, max);
 }
 
-int rng_lfsr_16bit_next(unsigned short *val, int max) {
+int rng_lfsr_16bit_next(uint16_t *val, int max) {
 	*val ^= (*val & 0x07ff) << 5;
 	*val ^= *val >> 7;
 	*val ^= (*val & 0x0003) << 14;
@@ -19,11 +20,11 @@ int rng_lfsr_16bit_next(unsigned short *val, int max) {
 }
 
 int rng16(int max) {
-	static unsigned short val = 1;
+	static uint16_t val = 1;
 	return rng_lfsr_16bit_next(&val, max);
 }
 
-int rng_lfsr_32bit_next(unsigned int *val, int max) {
+int rng_lfsr_32bit_next(uint32_t *val, int max) {
 	*val ^= (*val & 0x0007ffff) << 13;
 	*val ^= *val >> 17;
 	*val ^= (*val & 0x07ffffff) << 5;
@@ -31,7 +32,17 @@ int rng_lfsr_32bit_next(unsigned int *val, int max) {
 }
 
 int rng32(int max) {
-	static unsigned int val = 1;
+	static uint32_t val = 1;
 	return rng_lfsr_32bit_next(&val, max);
 }
 
+/*
+	32 bit rng lfsr does not seem to be working
+	too many repeats in rng_test
+
+	solution found here:
+	https://stackoverflow.com/questions/65661856/how-to-do-an-8-bit-16-bit-and-32-bit-linear-feedback-shift-register-prng-in-ja
+
+	should look more into Xorshift:
+	https://en.wikipedia.org/wiki/Xorshift
+*/
