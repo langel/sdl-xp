ce730229daa1db2da27467b321f6305f1b2f64cb
langel <knoxburry@gmail.com>
2021-06-15 18:29:18 -0400

    420 sine working but no key readings

--- main.c
+++ main.c
@@ -6,13 +6,20 @@ int SCREEN_WIDTH = 600;
 int SCREEN_HEIGHT = 800;
 int FPS = 60;
 
+const double TAO = M_PI * 2;
+
+// common sample rates:
+uint32_t sample_freq_common[] = { 8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000, 96000, 192000 };
+uint16_t sine_freq = 420;
 float samples_per_sine;
 
-void audio_callback(void* userdata, Uint8* buffer, int len) {
-	//printf(" %d ", len);
+void audio_callback(void* userdata, uint8_t* byte_stream, int byte_stream_length) {
 	static long pos = 0;
-	for (int i = 0; i < len; ++i) {
-		buffer[i] = sin((pos / samples_per_sine) * M_PI * 2);
+	float* float_stream = (float*) byte_stream;
+	uint32_t i;
+	uint32_t float_stream_length = byte_stream_length >> 2;
+	for (i = 0; i < float_stream_length; ++i) {
+		float_stream[i] = sin((pos / samples_per_sine) * TAO) * 0.25;
 		pos++;
 	}
 }
@@ -36,16 +43,17 @@ void log_spec(SDL_AudioSpec *as) {
 
 int main(int argc, char *argv[]) {
 
-	SDL_Init(SDL_INIT_AUDIO);
-	SDL_Event event;
+	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_AudioSpec want, have;
 	SDL_AudioDeviceID audio_device;
 
+	uint32_t sample_freq = sample_freq_common[0];
+
 	SDL_memset(&want, 0, sizeof(want));
-	want.freq = 48000;
-	want.format = AUDIO_F32;
-	want.channels = 2;
+	want.freq = sample_freq;
+	want.format = AUDIO_F32SYS;
+	want.channels = 1;
 	want.samples = 1024;
 	want.callback = audio_callback;
 	audio_device = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
@@ -56,19 +64,41 @@ int main(int argc, char *argv[]) {
 	log_spec(&want);
 	printf(" have spec:\n");
 	log_spec(&have);
+	sample_freq = have.freq;
 
 	SDL_Delay(42);
 	//SDL_LockAudioDevice(audio_device);
 	SDL_PauseAudioDevice(audio_device, 0);
-	samples_per_sine = (float)have.freq / 420.0;
 
+	SDL_Event event;
 	int running = 1;
+
 	while (running) {
+
+		samples_per_sine = (float)sample_freq / (float)sine_freq;
 	
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)) {
-				running = 0;
+			switch (event.type) {
+				case SDL_QUIT:
+					running = 0;
+					break;
+				case SDL_KEYDOWN:
+					printf( ", Name: %s", SDL_GetKeyName( event.key.keysym.sym ) );
+					printf(" keydown \n");
+					switch (event.key.keysym.sym) {
+						case SDLK_ESCAPE:
+							running = 0;
+							break;
+						case SDLK_UP:
+							sine_freq++;
+							break;
+						case SDLK_DOWN:
+							sine_freq--;
+							break;
+					}
+					break;
 			}
+			SDL_Delay(1);
 		}
 
 	}
