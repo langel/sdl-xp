ee149457285f5a2914d28f59073306b7972d9691
langel <knoxburry@gmail.com>
2021-10-24 18:57:15 -0400

    trying to knob

--- main.c
+++ main.c
@@ -1,84 +1,33 @@
-#include <stdio.h>
 #include <SDL2/SDL.h>
-
-#include "char_roms/_lib.c"
-
-#define ent_count 1024
 
 int window_w = 1200;
 int window_h = 800;
 
-struct char_ent {
-	int x_dir;
-	int x_pos;
-	int y_dir;
-	int y_pos;
-	int speed;
-};
+int main(int argc, char* args[]) {
 
-struct char_ent entities[ent_count];
-
-void entity_update(struct char_ent * ent) {
-	if (ent->x_dir) {
-		ent->x_pos += ent->speed;
-		if (ent->x_pos >= window_w) {
-			ent->x_dir = 0;
-		}
-	}
-	else {
-		ent->x_pos -= ent->speed;
-		if (ent->x_pos <= 0) {
-			ent->x_dir = 1;
-		}
-	}
-	if (ent->y_dir) {
-		ent->y_pos += ent->speed;
-		if (ent->y_pos >= window_h) {
-			ent->y_dir = 0;
-		}
-	}
-	else {
-		ent->y_pos -= ent->speed;
-		if (ent->y_pos <= 0) {
-			ent->y_dir = 1;
-		}
-	}
-}
-
-int main(int argc, char * argv[]) {
-
-	SDL_Init(SDL_INIT_VIDEO);
-
+	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Event event;
 	SDL_Window * window = SDL_CreateWindow("watttt", 200, 200,
 		window_w, window_h, SDL_WINDOW_RESIZABLE);
 	SDL_Renderer * renderer = SDL_CreateRenderer(window,
 		-1, SDL_RENDERER_PRESENTVSYNC);
 
-	struct char_rom font = char_rom_create_texture(renderer, "char_roms/eagle_pc_cga.bin");
-	SDL_Rect font_get = { 0, 0, window_w, 8 };
-	SDL_Rect temp_set = { 100, 100, 16, 16 };
-
-	for (int i = 0; i < ent_count; i++) {
-		entities[i].x_dir = i % 2;
-		entities[i].x_pos = i * 4;
-		entities[i].y_dir = (i % 3) - 1;
-		entities[i].y_pos = i * 2;
-		entities[i].speed = (i % 5) + 1;
+	const char * knob_path = "assets/knob-small.bmp";
+	SDL_Surface * knob_surface = SDL_LoadBMP(knob_path);
+	if (!knob_surface) {
+		printf("Failed to load image at %s: %s\n", knob_path, SDL_GetError());
+		return 1;
 	}
-
+	SDL_Texture * knob_texture = SDL_CreateTextureFromSurface(renderer, knob_surface);
+	SDL_SetTextureBlendMode(knob_texture, SDL_BLENDMODE_BLEND);
+	SDL_FreeSurface(knob_surface);
+	
 	int running = 1;
 	while (running) {
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 		SDL_RenderClear(renderer);
-		for (int i = 0; i < ent_count; i++) {
-			entity_update(&entities[i]);
-			font_get = char_rom_get_rect(font, i % 256);
-			temp_set.x = entities[i].x_pos;
-			temp_set.y = entities[i].y_pos;
-			SDL_RenderCopy(renderer, font.texture, &font_get, &temp_set);
-		}
+		SDL_RenderCopy(renderer, knob_texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
 
 		while (SDL_PollEvent(&event)) {
