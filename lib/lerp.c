9b2c64cdbc1dc9e40f750ece478c49fcdfed0508
Langel <knoxburry@gmail.com>
2022-09-06 22:34:44 -0400

    added cubic lerps

--- lib/lerp.c
+++ lib/lerp.c
@@ -1,4 +1,6 @@
 
+// LINEAR INTERPOLATION
+
 // imprecise; can perform floating point wonkiness
 float lerp(float a, float b, float t) {
 	return a + (b - a) * t;
@@ -20,3 +22,35 @@ float lerp2dp(float a, float b, float c, float d, float tx, float ty) {
 }
 
 
+// CUBIC INTERPOLATION
+
+// ripped and transcoded from https://www.paulinternet.nl/?page=bicubic
+// imprecise cubic linear (1D)
+float lerpcubic(float p[4], float x) {
+	return p[1] + 0.5 * 
+		x * (p[2] - p[0] + 
+			x * (2.0*p[0] - 5.0*p[1] + 4.0*p[2] - p[3] + 
+				x * (3.0*(p[1] - p[2]) + p[3] - p[0])
+			)
+		);
+}
+
+// imprecise bicubic linear (2D)
+float lerpbicubic(float p[4][4], float x, float y) {
+	float arr[4];
+	arr[0] = lerpcubic(p[0], y);
+	arr[1] = lerpcubic(p[1], y);
+	arr[2] = lerpcubic(p[2], y);
+	arr[3] = lerpcubic(p[3], y);
+	return lerpcubic(arr, x);
+}
+
+// imprecise tricubic linear (3D)
+float lerptricubic(float p[4][4][4], float x, float y, float z) {
+	float arr[4];
+	arr[0] = lerpbicubic(p[0], y, z);
+	arr[1] = lerpbicubic(p[1], y, z);
+	arr[2] = lerpbicubic(p[2], y, z);
+	arr[3] = lerpbicubic(p[3], y, z);
+	return lerpcubic(arr, x);
+}
